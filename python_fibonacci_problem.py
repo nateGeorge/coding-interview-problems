7dc5c23f221add3800fd7361efd3b548c0952c53
wordsforthewise <wordsforthewise@gmail.com>
2016-09-01 13:04:44 -0600

    feat: rename file and autopep8

--- python_fibonacci_problem.py
+++ python_fibonacci_problem.py
@@ -1,12 +1,14 @@
 from timeit import timeit
 
+
 def fib(x):
     if not isinstance(x, int):
         raise ValueError("x must be an int")
     a, b = 0, 1
     for i in range(x):
-    a, b = b, a+b
+        a, b = b, a + b
     return a
+
 
 def rec_fib(n):
     '''inefficient recursive function as defined, returns Fibonacci number'''
@@ -14,17 +16,18 @@ def rec_fib(n):
     #print('n=', n)
     if n > 1:
         #print(n-1, n-2)
-        return rec_fib(n-1) + rec_fib(n-2)
-    #if n == 1:
+        return rec_fib(n - 1) + rec_fib(n - 2)
+    # if n == 1:
     #    print('adding 1')
     return n
+
 
 def mem_fib(n, _cache={}):
     '''efficiently memoized recursive function, returns a Fibonacci number'''
     if n in _cache:
         return _cache[n]
     elif n > 1:
-        return _cache.setdefault(n, mem_fib(n-1) + mem_fib(n-2))
+        return _cache.setdefault(n, mem_fib(n - 1) + mem_fib(n - 2))
     return n
 
 timeit("fib(20)", setup='from __main__ import fib', number=10)
